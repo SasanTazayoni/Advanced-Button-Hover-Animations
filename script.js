b9fd01f91242c35864e1519e08df697e6c399180
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-10-23 17:24:35 +0100

    Add ripple effect button

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ cursorLightenButton.onmousemove = (e) => {
   cursorLightenButton.style.setProperty("--x", x + "px");
   cursorLightenButton.style.setProperty("--y", y + "px");
 };
+
+// Ripple effect
+
+const rippleButton = document.querySelector(".ripple-button");
+
+function createRipple(e) {
+  let x = e.clientX - e.target.getBoundingClientRect().left;
+  let y = e.clientY - e.target.getBoundingClientRect().top;
+  let ripples = document.createElement("span");
+  ripples.style.left = x + "px";
+  ripples.style.top = y + "px";
+  this.appendChild(ripples);
+
+  setTimeout(() => {
+    ripples.remove();
+  }, 600);
+}
+
+rippleButton.addEventListener("mouseover", createRipple);
