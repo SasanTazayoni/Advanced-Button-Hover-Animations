7561b56dd828e1c70f1de7c0487b59df9319d688
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-10-26 22:05:41 +0100

    Improved cursor lighten button and renamed

---
+++ effects/Trail/trail.js
@@ -0,0 +1,43 @@
+export const initializeTrail = (button) => {
+  button.onmousemove = (e) => {
+    let x = e.pageX - button.offsetLeft;
+    let y = e.pageY - button.offsetTop;
+
+    button.style.setProperty("--x", x + "px");
+    button.style.setProperty("--y", y + "px");
+
+    createFadingCircle(button, e);
+  };
+};
+
+const createFadingCircle = (button, e) => {
+  const fadingCircle = document.createElement("div");
+  fadingCircle.className = "trail";
+  fadingCircle.style.left = `${
+    e.pageX - button.getBoundingClientRect().left
+  }px`;
+  fadingCircle.style.top = `${e.pageY - button.getBoundingClientRect().top}px`;
+  fadingCircle.style.transform = "translate(-50%, -50%)";
+  button.appendChild(fadingCircle);
+  fadeOut(fadingCircle);
+};
+
+const fadeOut = (circle) => {
+  let opacity = 1;
+  const duration = 600;
+  const fadeInterval = 20;
+  const fadeOutStep = fadeInterval / duration;
+
+  const fadeEffect = setInterval(() => {
+    opacity -= fadeOutStep;
+    if (opacity <= 0) {
+      clearInterval(fadeEffect);
+      circle.remove();
+    } else {
+      circle.style.opacity = opacity;
+    }
+  }, fadeInterval);
+};
+
+const button = document.querySelector(".trail-button");
+initializeTrail(button);
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-import { initializeCursorLighten } from "./effects/Cursor lighten/cursorLighten.js";
+import { initializeTrail } from "./effects/Trail/trail.js";
 import { initializeRippleEffect } from "./effects/Ripple/ripple.js";
 import { initializeCircleEffect } from "./effects/Circle/circle.js";
 import { initializePixelationEffect } from "./effects/Pixel/pixel.js";
 
-const cursorLightenButton = document.querySelector(".cursor-lighten-button");
+const trailButton = document.querySelector(".trail-button");
 const rippleButton = document.querySelector(".ripple-button");
 const circleButton = document.querySelector(".circle-button");
 const pixelButton = document.querySelector(".pixel-button");
 
-initializeCursorLighten(cursorLightenButton);
+initializeTrail(trailButton);
 initializeRippleEffect(rippleButton);
 initializeCircleEffect(circleButton);
 initializePixelationEffect(pixelButton);
