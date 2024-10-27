e1333fc1dd4e9c67b9ac6c01b303faa2e2263874
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-10-27 15:19:43 +0000

    Add vortex button

--- script.js
+++ script.js
@@ -12,3 +12,52 @@ initializeTrail(trailButton);
 initializeRippleEffect(rippleButton);
 initializeCircleEffect(circleButton);
 initializePixelationEffect(pixelButton);
+
+function addVortexLines(button) {
+  const lineHeight = 3;
+  const numberOfLines = 28;
+
+  for (let i = 0; i < numberOfLines; i++) {
+    const line = document.createElement("span");
+    line.classList.add("line", "bottom-left-line");
+    button.appendChild(line);
+    const halfLineWidth = line.offsetWidth / 2;
+    line.style.bottom = `${i * lineHeight}px`;
+    line.style.left = `${-halfLineWidth + i * lineHeight}px`;
+    line.style.transitionDelay = `${0.02 * i}s`;
+  }
+
+  for (let i = 0; i < numberOfLines; i++) {
+    const line = document.createElement("span");
+    line.classList.add("line", "bottom-right-line");
+    button.appendChild(line);
+    const halfLineWidth = line.offsetWidth / 2;
+    const buttonWidth = button.offsetWidth;
+    line.style.bottom = `${i * lineHeight}px`;
+    line.style.left = `${buttonWidth - halfLineWidth - i * lineHeight}px`;
+    line.style.transitionDelay = `${0.02 * i}s`;
+  }
+
+  for (let i = 0; i < numberOfLines; i++) {
+    const line = document.createElement("span");
+    line.classList.add("line", "top-left-line");
+    button.appendChild(line);
+    const halfLineWidth = line.offsetWidth / 2;
+    line.style.top = `${i * lineHeight}px`;
+    line.style.left = `${-halfLineWidth + i * lineHeight}px`;
+    line.style.transitionDelay = `${0.02 * i}s`;
+  }
+
+  for (let i = 0; i < numberOfLines; i++) {
+    const line = document.createElement("span");
+    line.classList.add("line", "top-right-line");
+    button.appendChild(line);
+    const halfLineWidth = line.offsetWidth / 2;
+    const buttonWidth = button.offsetWidth;
+    line.style.top = `${i * lineHeight}px`;
+    line.style.left = `${buttonWidth - halfLineWidth - i * lineHeight}px`;
+    line.style.transitionDelay = `${0.02 * i}s`;
+  }
+}
+
+document.querySelectorAll(".vortex-button").forEach(addVortexLines);
